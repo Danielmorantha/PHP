29a6a15fc433f2d652033176f285fd008478f86e
Daniel Morantha <91647127+Danielmorantha@users.noreply.github.com>
2024-07-02 08:24:53 +0700

    Delete webSiPerpus directory

--- webSiPerpus/web/admin/PeminjamBuku_hapus_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-	 include "../konekDB.php";
-	 $qPinjamBuku = "DELETE FROM pinjam_buku WHERE id_pinjam = $_REQUEST[id_pinjam]";
-	 $qPinjamBukuHasil = mysqli_query($KoneksiDB, $qPinjamBuku);
-	 mysqli_commit($KoneksiDB);
-	 header("location: PeminjamBuku.php");
-?>
--- webSiPerpus/web/admin/akun_hapus.php
+++
@@ -1,7 +0,0 @@
-<?php
-	 include "../konekDB.php";
-	 $qAkun = "DELETE FROM pengguna WHERE id_pengguna = $_REQUEST[id_pengguna]";
-	 $qAkunHasil = mysqli_query($KoneksiDB, $qAkun);
-	 mysqli_commit($KoneksiDB);
-	 header("location: akun.php");
-?>
